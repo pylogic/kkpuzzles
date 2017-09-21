9cebaaea6a4a6183ca8f70bbf98509a5b97dab43
ganben <caozhao@gmail.com>
2017-09-21 23:28:45 +0800

    q2

--- k-n-k/script10.py
+++ k-n-k/script10.py
@@ -15,41 +15,60 @@ from z3 import *
 kt, kv= Ints('kt kv')
 zoey, mel = Ints('zoey mel')
 #check if zoey == 1 == kt;
-s0 = Solver()
+s = Solver()
 #s0.add(Distinct(zoey, mel))
 #constrains 1, or 2 1 == kt, 2 == kv
-s0.add(zoey>=1, zoey<=2)
-s0.add(mel>=1,  mel<=2)
+s.add(zoey>=1, zoey<=2)
+s.add(mel>=1,  mel<=2)
 #add assume:
-s0.add(zoey == 1, mel == 2)
+s.add(zoey == 1, mel == 2)
 #add sayings
-s0.add(mel==2)
-s0.add(Xor(mel <> 2, zoey <> 2)) #negation due to 2
-r = s0.check()
+s.add(mel==2)
+s.add(Xor(mel <> 2, zoey <> 2)) #negation due to 2
+r = s.check()
 
 if r == unsat:
-    print('zoey mel <> 1 2')
+    print('Q1: zoey mel <> 1 2')
 
 else:
-    print(s0.model())
+    print('Q1: zoey = 1, mel = 2')
+    print(s.model())
 
 #assume 2: zoey == 2 == kv
-s1 = Solver()
+s = Solver()
 #s1.add(Distinct(zoey, mel))
 #constrains 1, or 2 1 == kt, 2 == kv
-s1.add(zoey>=1, zoey<=2)
-s1.add(mel>=1,  mel<=2)
+s.add(zoey>=1, zoey<=2)
+s.add(mel>=1,  mel<=2)
 #add assumes
-s1.add(zoey ==2, mel ==1)
+s.add(zoey ==2, mel ==1)
 #add sayings
-s1.add(mel <> 2) # negation due to 2
-s1.add(mel <> 2, zoey <>2)
-r = s1.check()
+s.add(mel <> 2) # negation due to 2
+s.add(mel <> 2, zoey <>2)
+r = s.check()
 if r == unsat:
-    print('zoey mel <> 2 1')
+    print('Q1: zoey mel <> 2 1')
 else:
-    print(s1.model())
+    print(s.model())
 
 
 #Q2
+# Peggy tells you that “of Zippy and I, exactly one is a knight'.
+# Zippy tells you that only a knave would say that Peggy is a knave.
+peggy, zippy = Ints('peggy zippy')
 
+s = Solver()
+s.add(zoey>=1, zoey<=2)
+s.add(mel>=1,  mel<=2)
+
+#cases: p, z = [1 1] [1 2] [2 1] [2 2]
+
+#cases p, z = [1 2] or [2 1]
+#s.add(Distinct(peggy, zippy))
+s.add(Or((peggy == 1 and zippy == 2), (peggy == 2 and zippy == 2)))
+s.add(Or((peggy == 2 and zippy == 2), (peggy == 1 and zippy == 1)))
+r = s.check()
+if r == unsat:
+    print('Q2: ??')
+else:
+    print(s.model())
